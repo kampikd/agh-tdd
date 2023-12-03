93044636efd7e7380a68474a045ab3d8ea607dad
Damian Kampik <damian.kampik@u2i.com>
2023-12-03 23:40:38 +0100

    Fix failing tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,14 +1,16 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @user = User.create!(email: 'test1@test.com', password: 'testest', password_confirmation: 'testest')
     @post = Post.new(title: 'title', content: 'content', user: @user)
+    sign_in @user
   end
 
   test "should create post v1" do
     assert_difference("Post.count") do
-      post posts_url, params: { post: { content: @post.content, title: @post.title, user_id: @post.user_id } }
+      post posts_url, params: { post: { content: @post.content, title: @post.title } }
     end
 
     assert_redirected_to post_url(Post.last)
@@ -17,16 +19,12 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should create post v2" do
     assert Post.count == 0
 
-    post posts_url, params: { post: { content: @post.content, title: @post.title, user_id: @post.user_id } }
+    post posts_url, params: { post: { content: @post.content, title: @post.title } }
 
     assert Post.count == 1
-    assert post == @post
+    assert Post.last.title == @post.title
+    assert Post.last.content == @post.content
 
     assert_redirected_to post_url(Post.last)
-  end
-
-  teardown do
-    Post.destroy_all
-    User.destroy_all
   end
 end
